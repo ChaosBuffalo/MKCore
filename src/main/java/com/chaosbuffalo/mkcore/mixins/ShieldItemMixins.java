bea440f1743eee373d760b5f1799c7ef956617de
Kovak <kovac1066@gmail.com>
2021-12-23 22:24:31 -0800

    annotate these overrides/overwrites differently so they work in client

--- src/main/java/com/chaosbuffalo/mkcore/mixins/ShieldItemMixins.java
+++ src/main/java/com/chaosbuffalo/mkcore/mixins/ShieldItemMixins.java
@@ -8,12 +8,17 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(ShieldItem.class)
 public abstract class ShieldItemMixins {
 
 
-    //  shield can't block if we're poise broke
+    /**
+     * @author kovak
+     * @reason shield can't block if we're poise broke
+     */
+    @Overwrite
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn){
         ItemStack itemstack = playerIn.getHeldItem(handIn);
         if (MKCore.getPlayer(playerIn).map(x -> x.getStats().isPoiseBroke()).orElse(false)){
--- src/main/java/com/chaosbuffalo/mkcore/mixins/SwordItemMixins.java
+++ src/main/java/com/chaosbuffalo/mkcore/mixins/SwordItemMixins.java
@@ -2,29 +2,43 @@ package com.chaosbuffalo.mkcore.mixins;
 
 import com.chaosbuffalo.mkcore.MKCore;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ShieldItem;
-import net.minecraft.item.SwordItem;
-import net.minecraft.item.UseAction;
+import net.minecraft.item.*;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(SwordItem.class)
-public class SwordItemMixins {
+public abstract class SwordItemMixins extends TieredItem {
 
-    // change sword to block when used
+    public SwordItemMixins(IItemTier tierIn, Properties builder) {
+        super(tierIn, builder);
+    }
+
+    /**
+     * @author kovak
+     * @reason change sword to block when used
+     */
+    @Override
     public UseAction getUseAction(ItemStack stack) {
         return UseAction.BLOCK;
     }
 
-    // give use duration same as shield
+    /**
+     * @author kovak
+     * @reason give use duration same as shield
+     */
+    @Override
     public int getUseDuration(ItemStack stack) {
         return 72000;
     }
 
-    // make sword block only when we are not poise broke and shield is not in offhand
+
+    /**
+     * @author kovak
+     * @reason make sword block only when we are not poise broke and shield is not in offhand
+     */
+    @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
         ItemStack itemstack = playerIn.getHeldItem(handIn);
         ItemStack offhand = playerIn.getHeldItemOffhand();
